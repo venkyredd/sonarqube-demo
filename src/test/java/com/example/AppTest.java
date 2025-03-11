502f6655c09ee8f3052eb530c0cfc24345d267c8
root <root@LAPTOP-0FERN5QT.>
2025-03-11 06:17:23 +0000

    Added Java project for SonarQube

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testMain() {
+        assertEquals(1, 1);
+    }
+}
